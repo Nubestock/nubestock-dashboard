0728ab875a1bf46af48fc617c1493bb073c1db18
Jeremy León <jeremy.leon.g@outlook.com>
2026-02-16 14:41:09 -0500

    chore: add @types/node to devDependencies and enhance Vite configuration with custom build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,32 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) return 'vendor'
+          return undefined
+        },
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+  },
   resolve: {
     alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      '@': './src',
     },
   },
   server: {
-    // Configurar para que todas las rutas redirijan a index.html (SPA)
     proxy: {},
-    historyApiFallback: true,
   },
 })
